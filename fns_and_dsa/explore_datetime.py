9d857303b7f6b91fb4d51872a8474e7fc90c778c
Antony Gitee <antonygitee@gmail.com>
2025-07-25 22:27:20 +0300

    Fix explore_datetime with correct functions and formatting

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current date and time:", formatted_date)
+
+def calculate_future_date():
+    days = int(input("Enter the number of days to add to the current date: "))
+    future_date = datetime.now() + timedelta(days=days)
+    formatted_future = future_date.strftime("%Y-%m-%d")
+    print("Future date:", formatted_future)
+
+# Main program logic
+display_current_datetime()
+calculate_future_date()
