d146f71afa1d965bdbf589686662b5046c72908b
Antony Gitee <antonygitee@gmail.com>
2025-07-25 19:51:42 +0300

    Save updates to basic_operations and add control-flow scripts

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,13 @@
+# Assign values to variables 
 number1 = 10
 number2 = 5
 
+#Perfom arithmetic operations
 sum_result = number1 + number2
 difference_result = number1 - number2
 product_result = number1 * number2
 
+#Print the results in human-readable format
 print(f"Addition of {number1} and {number2} is {sum_result}")
 print(f"Subtraction of {number1} and {number2} is {difference_result}")
 print(f"Multiplication of {number1} and {number2} is {product_result}")
